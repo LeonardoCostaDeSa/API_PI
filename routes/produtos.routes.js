4f1317f0dbb22d10803a67ef1f647274ef3bb379
jvgomid3 <161902697+jvgomid3@users.noreply.github.com>
2024-06-02 10:07:50 -0300

    Update produtos.routes.js

--- routes/produtos.routes.js
+++ routes/produtos.routes.js
@@ -1,1 +1,16 @@
-//
+import { Router } from 'express'
+import ProdutoController from '../controllers/produto.controller.js'
+import { 
+    createProdutoValidator, updateProdutoValidator, deleteProdutoValidator 
+    } from '../validators/produto.validator.js'
+
+const router = Router()
+
+router.get('/', ProdutoController.index)
+router.post('/', createProdutoValidator, ProdutoController.create)
+router.get('/:id', ProdutoController.show)
+router.put('/:id', updateProdutoValidator, ProdutoController.update)
+router.delete('/:id', deleteProdutoValidator, ProdutoController.delete)
+// ...
+
+export default router
