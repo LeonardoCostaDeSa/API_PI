3d6207a8530f10665888e37ff2851c95aca9d2a3
jvgomid3 <161902697+jvgomid3@users.noreply.github.com>
2024-06-02 15:22:05 -0300

    Update produtos.validator.js

--- validators/produtos.validator.js
+++ validators/produtos.validator.js
@@ -3,22 +3,22 @@ import { body, param } from 'express-validator'
 export const createProdutosValidator = [
     body('descricao').isString().withMessage('Descrição inválida'),
     body('classe').isString().withMessage('Classe inválida'),
-    body('unidade').isInt().withMessage('Unidade inválida'), // verificar se é String ou algo relacionado a Number (alterei de String para Int)
-    body('fornecedorId').isString().withMessage('ID de fornecedor inválido'), // verificar tipo isString
+    body('unidade').isInt().withMessage('Unidade inválida'),
+    body('fornecedorId').isInt().withMessage('ID de fornecedor inválido'),
     body('fornecedor').isString().withMessage('Fornecedor inválido'),
-    body('imagemUrl').isString().withMessage('Imagem inválida'), // verificar tipo isString > Imagem ou URL
-    body('ncm').isString().isPostalCode('BR').withMessage('NCM inválido'), // verificar tipo isString
+    body('imagemUrl').isString().withMessage('Imagem inválida'),
+    body('ncm').isString().withMessage('BR').withMessage('NCM inválido'),
     body('orcamentos').isString().withMessage('Orçamento inválido'),
   ];
 
 export const updateProdutosValidator = [
     body('descricao').isString().withMessage('Descrição inválida'),
     body('classe').isString().withMessage('Classe inválida'),
-    body('unidade').isInt().withMessage('Unidade inválida'), // verificar se é String ou algo relacionado a Number (alterei de String para Int)
-    body('fornecedorId').isString().withMessage('ID de fornecedor inválido'), // verificar tipo isString
+    body('unidade').isInt().withMessage('Unidade inválida'),
+    body('fornecedorId').isInt().withMessage('ID de fornecedor inválido'),
     body('fornecedor').isString().withMessage('Fornecedor inválido'),
-    body('imagemUrl').isString().withMessage('Imagem inválida'), // verificar tipo isString > Imagem ou URL
-    body('ncm').isString().isPostalCode('BR').withMessage('NCM inválido'), // verificar tipo isString
+    body('imagemUrl').isString().withMessage('Imagem inválida'),
+    body('ncm').isString().withMessage('BR').withMessage('NCM inválido'),
     body('orcamentos').isString().withMessage('Orçamento inválido'),
 ]
 
