b28ffbf0867de496efcb78e74e616da36324e19e
Adriana-Araujo <adriana.araujo6@fatec.sp.gov.br>
2024-06-02 12:49:51 -0300

    Routes and Validator

--- routes/fornecedor.routes.js
+++ routes/fornecedor.routes.js
@@ -1,1 +1,16 @@
-//
+import { Router } from 'express'
+import FornecedorController from '../controllers/fornecedor.controller.js'
+import { 
+    createFornecedorValidator, updateFornecedorValidator, deleteFornecedorValidator 
+    } from '../validators/fornecedor.validator.js'
+
+const router = Router()
+
+router.get('/', FornecedorController.index)
+router.post('/', createFornecedorValidator, FornecedorController.create)
+router.get('/:id', FornecedorController.show)
+router.put('/:id', updateFornecedorValidator, FornecedorController.update)
+router.delete('/:id', deleteFornecedorValidator, FornecedorController.delete)
+// ...
+
+export default router
--- validators/fornecedor.validator.js
+++ validators/fornecedor.validator.js
@@ -1,1 +1,20 @@
-//
+import { body, param } from 'express-validator'
+
+export const createFornecedorValidator = [
+    body('razaoSocial').isString().withMessage('Razao inválida'),
+    body('telefone').isString().isLength({ min: 12, max: 14 }).withMessage('Telefone inválido'),
+    body('descricao').isString().withMessage('Descrição inválida'),
+    body('endereco').isString().withMessage('Endereço inválido'),
+    body('orcamento').isString().withMessage('Orçamento inválido'),
+  ];
+
+export const updateFornecedorValidator = [
+    body('razaoSocial').isString().withMessage('Razao inválida'),
+    body('telefone').isString().isLength({ min: 12, max: 14 }).withMessage('Telefone inválido'),
+    body('descricao').isString().withMessage('Descrição inválida'),
+    body('endereco').isString().withMessage('Endereço inválido'),
+    body('orcamento').isString().withMessage('Orçamento inválido'),
+]
+export const deleteFornecedorValidator = [
+  param('id').isInt().withMessage("Id inválido"),
+]
