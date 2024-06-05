283735fd0cd41ed7ce3e22e95b0757517b58989c
Renato Carvalho <renato.itdevcarvalho@gmail.com>
2024-06-04 21:58:57 -0300

    Controller, model e routes da entidade orçamento

--- controllers/orcamento.controller.js
+++ controllers/orcamento.controller.js
@@ -1,1 +1,86 @@
-//
+import Orcamento from '../models/orcamento.model.js'
+import { validationResult } from 'express-validator'
+
+export default class OrcamentoController {
+    static async index(req, res) {
+        const orcamento = await Orcamento.findMany()
+        res.json(orcamento)
+    }
+    static async create(req, res) {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const orcamento = await Orcamento.create({
+            data: req.body
+        })
+        res.json(orcamento)
+    }
+
+    static async show(req, res) {
+        const orcamento = await Orcamento.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        if (!orcamento) {
+            return res.status(404).json({ message: 'Orçamento não encontrado' })
+        }
+        res.json(orcamento)
+    }
+
+    static async update(req, res) {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const orcamento = await Orcamento.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        if (!orcamento) {
+            return res.status(404).json({ message: 'Orcamento não encontrado' })
+        }
+        const updatedOrcamento = await Orcamento.update({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            data: req.body
+        })
+        res.json(updatedOrcamento)
+    }
+
+    static async delete(req, res) {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const orcamento = await Orcamento.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        if (!orcamento) {
+            return res.status(404).json({ message: 'Orcamento não encontrado' })
+        }
+        await Orcamento.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        try {
+            await Orcamento.delete({
+                where: {
+                    id: parseInt(req.params.id)
+                }
+            })
+        } catch (error) {
+            return res.status(400).json({ message: "Erro não informado" })
+        }
+
+        res.status(204).json({ message: 'Orcamento deletado com sucesso' })
+
+    }
+
+}
--- routes/orcamento.routes.js
+++ routes/orcamento.routes.js
@@ -1,1 +1,15 @@
-//
+import { Router } from 'express'
+import OrcamentoController from '../controllers/orcamento.controller.js'
+import {
+    createOrcamentoValidator, updateOrcamentoValidator, deleteOrcamentoValidator
+} from '../validators/orcamento.validator.js'
+
+const router = Router()
+
+router.get('/', OrcamentoController.index)
+router.post('/', createOrcamentoValidator, OrcamentoController.create)
+router.get('/:id', OrcamentoController.show)
+router.put('/:id', updateOrcamentoValidator, OrcamentoController.update)
+router.delete('/:id', deleteOrcamentoValidator, OrcamentoController.delete)
+
+export default router
