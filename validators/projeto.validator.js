95f7fdb0a1ae61b32189203384d80b136e7870ba
Augusto Trindade <augusto.trindades@gmail.com>
2024-06-04 21:43:15 -0300

    Alterando Projeto Validator

--- validators/projeto.validator.js
+++ validators/projeto.validator.js
@@ -1,16 +1,15 @@
 import { body, param } from 'express-validator'
 
-//É necessário validar os campos de 'id' também?
+
 export const createProjetoValidator = [
-    //Atributo data entrou como DateTime. Mantém ou trocamos para String?
     param('id').isInt().withMessage("ID Inválido"),
-    body('data').isDate().withMessage("Data inválida"),
+    body('data').isString().withMessage("Data inválida"),
     body('concluido').isString().withMessage("Conteúdo inválido"),
 ]
 
 export const updateProjetoValidator = [
     param('id').isInt().withMessage("ID Inválido"),
-    body('data').isDate().withMessage("Data inválida"),
+    body('data').isString().withMessage("Data inválida"),
     body('concluido').isString().withMessage("Conteúdo inválido"),
 ]
 
