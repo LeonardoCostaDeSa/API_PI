ca4dc21ddc41ac0e5818f1b72a70c2a8ae44a776
Leonardo Costa de Sá <150630873+LeonardoCostaDeSa@users.noreply.github.com>
2024-06-12 00:18:58 +0000

    Cliente atualizado

--- src/validators/cliente.validator.js
+++ src/validators/cliente.validator.js
@@ -1,31 +1,31 @@
 import { body, param } from 'express-validator'
 
-export const createClienteValidator = [
-    body('nome').isString().withMessage('Nome inválido'),
-    body('cnpjCpf').isString().withMessage('CNPJ/CPF inválido'),
-    body('tipo').isString().withMessage('Tipo inválido'),
-    body('rua').isString().withMessage('Rua inválida'),
-    body('numero').isInt().withMessage('Número inválido'),
-    body('bairro').isString().withMessage('Bairro inválido'),
-    body('cep').isString().isPostalCode('BR').withMessage('CEP inválido'),
-    body('cidade').isString().withMessage('Cidade inválida'),
-    body('estado').isString().isLength({ min: 2, max: 2 }).withMessage('Estado inválido'),
-    body('telefone').isString().isMobilePhone('pt-BR').withMessage('Telefone inválido')
-  ];
+export const clienteValidator = [
+  body('nome').isString().withMessage('Nome inválido'),
+  body('cnpjCpf').isString().withMessage('CNPJ/CPF inválido'),
+  body('tipo').isString().withMessage('Tipo inválido'),
+  body('rua').isString().withMessage('Rua inválida'),
+  body('numero').isInt().withMessage('Número inválido'),
+  body('bairro').isString().withMessage('Bairro inválido'),
+  body('cep').isString().isPostalCode('BR').withMessage('CEP inválido'),
+  body('cidade').isString().withMessage('Cidade inválida'),
+  body('estado').isString().isLength({ min: 2, max: 2 }).withMessage('Estado inválido'),
+  body('telefone').isString().isMobilePhone('pt-BR').withMessage('Telefone inválido')
+];
 
-export const updateClienteValidator = [
-    body('nome').isString().withMessage('Nome inválido'),
-    body('cnpjCpf').isString().withMessage('CNPJ/CPF inválido'),
-    body('tipo').isString().withMessage('Tipo inválido'),
-    body('rua').isString().withMessage('Rua inválida'),
-    body('numero').isInt().withMessage('Número inválido'),
-    body('bairro').isString().withMessage('Bairro inválido'),
-    body('cep').isString().isPostalCode('BR').withMessage('CEP inválido'),
-    body('cidade').isString().withMessage('Cidade inválida'),
-    body('estado').isString().isLength({ min: 2, max: 2 }).withMessage('Estado inválido'),
-    body('telefone').isString().isMobilePhone('pt-BR').withMessage('Telefone inválido') 
+export const clienteUpdateValidator = [
+  body('nome').isString().withMessage('Nome inválido'),
+  body('cnpjCpf').isString().withMessage('CNPJ/CPF inválido'),
+  body('tipo').isString().withMessage('Tipo inválido'),
+  body('rua').isString().withMessage('Rua inválida'),
+  body('numero').isInt().withMessage('Número inválido'),
+  body('bairro').isString().withMessage('Bairro inválido'),
+  body('cep').isString().isPostalCode('BR').withMessage('CEP inválido'),
+  body('cidade').isString().withMessage('Cidade inválida'),
+  body('estado').isString().isLength({ min: 2, max: 2 }).withMessage('Estado inválido'),
+  body('telefone').isString().isMobilePhone('pt-BR').withMessage('Telefone inválido')
 ]
 
-export const deleteClienteValidator = [
+export const clienteIdValidator = [
   param('id').isInt().withMessage("Id inválido"),
 ]
