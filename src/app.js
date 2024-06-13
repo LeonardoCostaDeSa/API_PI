cacac5ef6f94c6e3cb1130cdbdd4f87b83eb11cc
Renato Carvalho <renato.itdevcarvalho@gmail.com>
2024-06-13 14:01:08 -0300

    Modificações para utilizar o Render

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import routes from './routes/index.js'
+import cors from 'cors'
 
 const app = express()
-const PORT = 3000 
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 app.use('/api', routes)
 
 app.listen(PORT, () => {
